78219fb9cc08462ce55903b7747e3de60a901c87
DuongPC <pduong244@gmail.com>
2023-08-17 22:15:43 +0700

    Relationships

--- src/service/userService.js
+++ src/service/userService.js
@@ -23,6 +23,20 @@ module.exports = {
         }
     },
     getAllUsers: async () => {
+        let user = await db.User.findOne({
+            where: { id: 1 },
+            attributes: ["id", "email", "username"],
+            include: { model: db.Group, attributes: ["name", "description"] },
+            raw: true,
+            nest: true
+        });
+        let roles = await db.Role.findAll({
+            include: { model: db.Group, where: { id: 1 } },
+            raw: true,
+            nest: true
+        });
+        console.log(user);
+        console.log(roles);
         let users = [];
         try {
             users = await db.User.findAll();
